f977d923972bea6785156825071ca9cc27859a72
saatarko <saatar@tut.by>
2024-08-05 21:46:21 +0300

    first

---
+++ app.py
@@ -0,0 +1,44 @@
+import asyncio
+
+import aiohttp
+from bs4 import BeautifulSoup
+from aiohttp import web
+
+
+async def get_current_usd_rate(session, url):
+    async with session.get(url) as response:
+        if response.status == 200:
+            html_content = await response.text()
+            soup = BeautifulSoup(html_content, 'html.parser')
+            return soup
+        elif response.status == 423:
+            print("Сайт заблокирован")
+            return None
+        else:
+            return print("К сожалению страница для парсинга недоступна")
+
+
+def parse_titles(soup):
+    # Ищем все div элементы, у которых класс содержит слово 'title'
+    titles = soup.select('div[class*="title"]')
+    return [title.get_text(strip=True) for title in titles]
+
+
+async def main():
+    # url = 'https://5element.by'
+    url = 'https://www.onliner.by'
+
+    async with aiohttp.ClientSession() as session:
+        soup = await get_current_usd_rate(session, url)
+        if soup:
+            titles = parse_titles(soup)
+            print(titles)
+        else:
+            print("На странице заголовков не найдено")
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except RuntimeError as e:
+        print(f"Ошибка: {e}")
