225d9f5c5a7d0c2ef5ca1e95ae932d1b8cb563a7
saatarko <saatar@tut.by>
2024-08-05 21:53:34 +0300

    first

--- app.py
+++ app.py
@@ -36,9 +36,13 @@ async def main():
         else:
             print("На странице заголовков не найдено")
 
+    await asyncio.sleep(1)
+
 
 if __name__ == '__main__':
     try:
         asyncio.run(main())
+
     except RuntimeError as e:
         print(f"Ошибка: {e}")
+
